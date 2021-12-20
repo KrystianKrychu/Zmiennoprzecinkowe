1d09353f750783135d532360ed90cc47095ebfe0
Krystian Wieczorek <krystianwieczorek70@gmail.com>
2021-12-20 21:16:32 +0100

    Commit

--- src/pl/akademia108/Main.java
+++ src/pl/akademia108/Main.java
@@ -1,9 +1,13 @@
 package pl.akademia108;
-
+import pl.akademia108.java.VariablesDeclarationInitialization;
 public class Main {
 
     public static void main(String[] args) {
-	
+        VariablesDeclarationInitialization
+
+
+
+
 
     }
 }
